8c3f4982f82ca7fba99040fc1e6b881e49445de1
EC2 Default User <ec2-user@ip-172-31-44-155.us-east-2.compute.internal>
2018-05-15 10:04:35 +0000

    post

---
+++ app/Http/Controllers/MicropostsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// 追加
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class MicropostsController extends Controller
+{
+    // 追加
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+            $data = [
+                'user' => $user,
+                'microposts' => $microposts,
+            ];
+            $data += $this->counts($user);
+            return view('users.show', $data);
+        }else {
+            return view('welcome');
+        }
+    }
+    
+     // 追加
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:255',
+        ]);
+
+        $request->user()->microposts()->create([
+            'content' => $request->content,
+        ]);
+
+        return redirect('/');
+    }
+    
+      // 追加
+    public function destroy($id)
+    {
+        $micropost = \App\Micropost::find($id);
+
+        if (\Auth::user()->id === $micropost->user_id) {
+            $micropost->delete();
+        }
+
+        return redirect()->back();
+    }
+
+}
---
+++ database/migrations/2018_05_15_141956_create_microposts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMicropostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('microposts', function (Blueprint $table) {
+            $table->increments('id');
+            // 追加
+            $table->integer('user_id')->unsigned()->index();   
+            $table->string('content');
+            
+            $table->timestamps();
+            
+             // 外部キー制約 追加
+            $table->foreign('user_id')->references('id')->on('users');
+            
+        });
+        
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('microposts');
+    }
+}
